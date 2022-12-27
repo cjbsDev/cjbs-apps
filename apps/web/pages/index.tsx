069e5976e54da9d09f5323b3d1524dc5338409c3
musongfedev <musong.choi@chunlab.com>
2022-12-27 15:33:15 +0900

    - elint 설정 - Storybook 추가

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,9 +3,11 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Websssss</h1>
       <Button />
-      New Feature test 1234%%!
+      <p>New Feature test 1234%%!</p>
+      <p>{process.env.NEXT_PUBLIC_API_URL}</p>
+      <p>{process.env.NEXT_PUBLIC_EZMX_ID_AES_KEY}</p>
     </div>
   );
 }
