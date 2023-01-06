7a31de751cf78085a05d0d8a15d0e2de46320e51
musongfedev <musong.choi@chunlab.com>
2023-01-06 14:09:02 +0900

    #Button 컴포넌트 button 기본 속성들을 별로도 정의 하지 않고 사용 할 수 있도록 수정

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Web() {
   return (
     <div>
       <h1>Websssss</h1>
-      <Button />
+      <Button onClick={() => console.log('click')}>ㄲㄲㄲㄲButton</Button>
       <p>New Feature test 1234%%!</p>
       {/*<p>{process.env.NEXT_PUBLIC_API_URL}</p>*/}
       {/*<p>{process.env.NEXT_PUBLIC_EZMX_ID_AES_KEY}</p>*/}
